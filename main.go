425949f19ce1d2222484dac3a1a126a309b13599
Hanniah-Heart <hanniah.inbox@gmail.com>
2026-07-31 09:47:13 -0400

    Commit 4: Initial Commands for REPL

--- main.go
+++ main.go
@@ -5,55 +5,68 @@ import (
 	"os"
 )
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	commandList := getCommandList()
+	fmt.Println("# Welcome to the Pokedex!")
+	fmt.Println()
+	fmt.Println("## Usage:")
+	fmt.Println()
+	for _, command := range commandList {
+		fmt.Printf("- %v: %v\n", command.name, command.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommandList() map[string]cliCommand {
+	commandList := map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Describe how to use the Pokedex",
+			callback: commandHelp,
+		}, "exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+	return commandList
+}
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	for true {
+		commandList := getCommandList()
 		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin) /*
-		Designate variable "scanner" to hold operating system's standard input (os.Stdin)
-		as read and sorted by the NewScanner function from the bufio import.
-
-		The Go bufio manual says this about the function NewScanner
-			"
-		func NewScanner
-		added in go1.1
-		func NewScanner(r io.Reader) *Scanner
-
-		NewScanner returns a new Scanner to read from r.
-		The split function defaults to ScanLines.
-			"
-
-		Therefore scanner is now a bufio designed Scanner which listen's to os.Stdin.
-		*/
-		scanner.Scan() /*
-		the .Scan() method has the following description on the Go bufio manual.
-
-		func (*Scanner) Scan ¶
-		added in go1.1
-		func (s *Scanner) Scan() bool
-
-			"
-		Scan advances the Scanner to the next token,
-		which will then be available through the Scanner.
-		Bytes or Scanner.Text method.
-		It returns false when there are no more tokens,
-		either by reaching the end of the input or an error.
-		After Scan returns false,
-		the Scanner.Err method will return any error that occurred during scanning,
-		except that if it was io.EOF, Scanner.Err will return nil.
-		Scan panics if the split function returns too many empty tokens
-		without advancing the input.
-		This is a common error mode for scanners.
-			"
-
-		Apparently these tokens can be entire lines of commandline text
-		that are only complete when sent.
-
-		So, effectively, this line should ask the process to wait until it recieves a full
-		"token" or, effectively, a command line input as taken from its source
-		(the standard input).
-		*/
-		output := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s", output[0])
+		if !scanner.Scan() {
+			fmt.Println("Reached End of File")
+			os.Exit(0)
+		}
+		if len(cleanInput(scanner.Text())) < 1 {
+			fmt.Println("Please enter a command")
+			continue
+		}
+		userCommand := (cleanInput(scanner.Text()))[0]
+		cmd, exists := commandList[userCommand]
+		if exists == false {
+			fmt.Print("Unknown command")
+		} else {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
 		fmt.Println()
 	}
 }
