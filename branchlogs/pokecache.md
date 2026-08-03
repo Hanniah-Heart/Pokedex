c007f3af1fb5f6fbe1f376e3effa10d4a9c80b1b
Hanniah-Heart <hanniah.inbox@gmail.com>
2026-08-03 16:59:25 -0400

    Commit C1: Setup to Develop Caching

---
+++ branchlogs/pokecache.md
@@ -0,0 +1,26 @@
+#Branchlog for Caching Branch
+This markdown file will be where I write what's going on in the commits I make while
+trying to write and debug my program to add the intended feature of this branch.
+
+I'm trying to follow the [feature requests of the Caching lesson](https://www.boot.dev/lessons/f2ba2b87-38fe-467c-abd7-14716f955169)
+and make "moving around the map" more snappy by implementing caching.
+
+##Commit History
+###Commit C1: Setup to Develop Caching
+This commit just moves things around and writes down what the lesson instructions tell me to do
+without making serious attempts to accomplish those instructions. I'm definitely adopting a
+workflow of stablizing code, documenting it, commiting all that, and working on the next step.
+Some of the moves are designed with that in mind.
+- Created internal directory
+- Created pokecache direcotry within internal directory
+- Created pokecahce.go in pokecache directory
+- Added cacheEntry struct
+- Added Cache struct
+- Added incomplete NewCache function
+- Added empty but commented Cache.Add method
+- Added empty but commented Cache.Get method
+- Added empty but commented Cache.reapLoop method
+- Added comments to advise direction of further development
+- Renamed changelog_pokeapi.md to pokeapi.md
+- Moved pokeapi.md to branchlogs directory
+- Started this branchlog
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,58 @@
+package pokecache
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+type Cache struct {
+	entry map[string]cacheEntry
+	mutex *sync.Mutex
+}
+
+func NewCache(interval time.Duration) {
+	// create new cache with configurable interval
+	NAME := Cache {
+		entry: , // ???
+		mutex: , // ???
+	}
+	NAME.reapLoop(interval)
+}
+
+func (cache Cache) Add(key string) []byte, bool {
+	// add a new entry to the cache
+	// should use a sync.mutex
+}
+
+func (cache Cache) Get(key string) []byte, bool {
+	// get an entry from the cache
+	// bool should be true if entry was found and false if it wasn't
+	// should use a sync.mutex
+}
+
+func (cache Cache) reapLoop(interval time.Duration) {
+	// after interval seconds, destroy associated cache
+	// should use a sync.mutex
+}
+
+/*
+
+5. Update your code that makes requests to the PokeAPI to use the cache. Create the cache once and reuse it in your PokeAPI request layer. If you already have the data for a given URL (which is our cache key) in the cache, you should use that instead of making a new request. Whenever you do make a request, you should add the response to the cache.
+6. Write at least 1 test for your cache package! The tip below should help you get started.
+7. Test your application manually to make sure that the cache works as expected. When you use the map command to get data for the first time there should be a noticeable waiting time. However, when you use mapb it should be instantaneous because the data for that page is already in the cache. Feel free to add some logging that informs you in the command line when the cache is being used.
+
+Tips
+Clearing the Cache
+You can use a time.Ticker inside a goroutine started by NewCache. In a loop like for range ticker.C { ... }, check the entries and remove any whose createdAt is older than the cache's interval.
+
+Running Tests
+You can run tests for all packages in a Go module by running go test ./... from the root of the module.
+
+*/
+
+
+
